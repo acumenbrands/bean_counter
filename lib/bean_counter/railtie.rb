6aa564ca4e82d6fa13d2f1d7fa501c5e8e09cd9f
James Christie <james.aaron.christie@gmail.com>
2013-12-23 13:02:40 -0600

    Fix logger assignment in Railtie

--- lib/bean_counter/railtie.rb
+++ lib/bean_counter/railtie.rb
@@ -1,32 +1,28 @@
 require 'bean_counter'
 
-module Rails
+module BeanCounter
 
-  module BeanCounter
+  class Railtie < Rails::Railtie
 
-    class Railtie < Rails::Railtie
+    def self.generator
+      config.respond_to?(:app_generators) ? :app_generators : :generators
+    end
 
-      def self.generator
-        config.respond_to?(:app_generators) ? :app_generators : :generators
-      end
+    def handle_configuration_error(error)
+      puts "There is a configuration error with bean_counter.yml"
+      puts error.message
+    end
 
-      def handle_configuration_error(error)
-        puts "There is a configuration error with bean_counter.yml"
-        puts error.message
-      end
+    config.bean_counter = ::BeanCounter::Config
 
+    initializer "bean_counter.configure" do
       ::BeanCounter::Logging.logger = Rails.logger
 
-      config.bean_counter = ::BeanCounter::Config
+      config_file = Rails.root.join('config', 'bean_counter.yml')
 
-      initializer "bean_counter.configure" do
-        config_file = Rails.root.join('config', 'bean_counter.yml')
-
-        if config_file.file?
-          ::BeanCounter.load!(config_file)
-        end
+      if config_file.file?
+        ::BeanCounter.load!(config_file)
       end
-
     end
 
   end
